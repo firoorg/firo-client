5cb22f653bda8351a181c53cced0b7f26396ece0
Jörn Röder <kontakt@joernroeder.de>
2018-06-17 01:22:15 +0200

    whitelisted vue-qrcode-component

--- .electron-vue/webpack.renderer.config.js
+++ .electron-vue/webpack.renderer.config.js
@@ -19,7 +19,10 @@ const { VueLoaderPlugin } = require('vue-loader')
  * that provide pure *.vue files that need compiling
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/webpack-configurations.html#white-listing-externals
  */
-let whiteListedModules = ['vue']
+let whiteListedModules = [
+    'vue',
+    'vue-qrcode-component'
+]
 
 let rendererConfig = {
     devtool: '#cheap-module-eval-source-map',
