baab1a39e729193fc59fbdd1221544a1b46d813e
Jörn Röder <kontakt@joernroeder.de>
2018-06-17 01:28:26 +0200

    added badge

---
+++ src/renderer/components/Badge/AttentionBadge.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="badge attention">
+        {{ amount }}
+    </div>
+</template>
+
+<script>
+  export default {
+      name: 'AttentionBadge',
+      props: {
+          amount: Number
+      }
+  }
+</script>
+
+<style lang="scss">
+    .badge.attention {
+        color: $color--dark;
+        background-color: transparent;
+        background: $gradient--green-bright;
+        vertical-align: text-top;
+        @include font-black()
+    }
+</style>
