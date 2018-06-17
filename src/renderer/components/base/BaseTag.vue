fd13ca9d0a74eb4a3623c7712a97b109f2eeaca4
Jörn Röder <kontakt@joernroeder.de>
2018-06-17 21:26:22 +0200

    added BaseTag

---
+++ src/renderer/components/base/BaseTag.vue
@@ -0,0 +1,27 @@
+<template>
+    <span :class="['tag', sizeClassName]">
+        <slot />
+    </span>
+</template>
+
+<script>
+    export default {
+        name: 'BaseTag',
+        props: {
+            size: {
+                type: String,
+                default: ''
+            }
+        },
+
+        computed: {
+            sizeClassName () {
+                return this.size ? 'tag-' + this.size : null
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
