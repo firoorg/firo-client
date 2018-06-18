6242d138c2b1e6da5ede42d0e768ab6587d4594e
Jörn Röder <kontakt@joernroeder.de>
2018-06-19 00:29:05 +0200

    tiny webpack fix

--- .electron-vue/webpack.renderer.config.js
+++ .electron-vue/webpack.renderer.config.js
@@ -168,7 +168,7 @@ let rendererConfig = {
             '~': path.join(__dirname, '../src/store'),
             'vue$': 'vue/dist/vue.esm.js'
         },
-        extensions: ['.js', '.vue', '.json', '.css', '.node']
+        extensions: ['.js', '.vue', '.json', '.css', '.scss', '.node']
     },
     target: 'electron-renderer'
 }
