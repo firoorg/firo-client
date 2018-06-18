f4362c58f26578d467f3f6838024e7c52075b8b3
Jörn Röder <kontakt@joernroeder.de>
2018-06-18 03:03:16 +0200

    added network config

---
+++ src/main/config/network.js
@@ -0,0 +1,18 @@
+const HOST = 'tcp://127.0.0.1'
+
+export default {
+    mainnet: {
+        host: HOST,
+        ports: {
+            publisher: 8451,
+            request: 5557
+        }
+    },
+    testnet: {
+        host: HOST,
+        ports: {
+            publisher: 8451,
+            request: 15558
+        }
+    }
+}
