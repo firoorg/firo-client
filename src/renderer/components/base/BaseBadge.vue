cb0572edbbe0ebbc061715ac0fecdee5d06ce7b0
Jörn Röder <kontakt@joernroeder.de>
2018-06-22 03:58:38 +0200

    added base badge

---
+++ src/renderer/components/base/BaseBadge.vue
@@ -0,0 +1,46 @@
+<template>
+    <span class="badge-wrap">
+        <slot />
+        <span class="badge" v-show="visible">{{ count }}</span>
+    </span>
+</template>
+
+<script>
+    export default {
+        name: 'BaseBadge',
+
+        props: {
+            visible: {
+                type: Boolean,
+                default: true
+            },
+            count: {
+                type: Number,
+                default: 0
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .badge-wrap {
+        position: relative;
+    }
+
+    .badge {
+        @include setType(2, $ms-down1);
+        background: $gradient--comet-dark-horizontal;
+        padding: emRhythm(0.25, $silent: true) emRhythm(0.75, $silent: true) emRhythm(0.35, $silent: true);
+        display: inline-block;
+        color: $color--white;
+        @include font-heavy();
+        border-radius: emRhythm(0.5, $silent: true);
+        text-align: center;
+
+        position: absolute;
+        right: -75%;
+        bottom: -18%;
+
+        pointer-events: none;
+    }
+</style>
