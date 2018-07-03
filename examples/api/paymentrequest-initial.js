e9649de8f1da7dfac56ebb76fd362ed8dda7094f
riordant <riordant@tcd.ie>
2018-07-03 17:56:43 +0100

    update testnet port

--- examples/api/paymentrequest-initial.js
+++ examples/api/paymentrequest-initial.js
@@ -1,7 +1,7 @@
 const zmq = require('zeromq')
 
 const requester = zmq.socket('req')
-requester.connect('tcp://127.0.0.1:15558')
+requester.connect('tcp://127.0.0.1:25557')
 
 // log out replies
 requester.on('message', (msg) => {
