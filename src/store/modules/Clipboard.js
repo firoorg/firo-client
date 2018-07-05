68b24744621952f412928d96265a56bd1cea85a0
Jörn Röder <kontakt@joernroeder.de>
2018-07-06 01:11:02 +0200

    added notified key to clipboard store

--- src/store/modules/Clipboard.js
+++ src/store/modules/Clipboard.js
@@ -1,19 +1,31 @@
 const state = {
     // currentDenominations: {},
     clipboardValue: '',
-    timestamp: null
+    timestamp: null,
+    notified: false
 }
 
 const mutations = {
     SET_CLIPBOARD (state, value) {
         state.clipboardValue = value
         state.timestamp = Date.now()
+        state.notified = false
+    },
+
+    SET_CLIPBOARD_NOTIFIED (state, value) {
+        state.notified = value
     }
 }
 
 const actions = {
     setClipboard ({ commit, state }, value) {
         commit('SET_CLIPBOARD', value)
+    },
+
+    markAsNotified ({ commit, state }) {
+        if (state.clipboardValue && !state.notified) {
+            commit('SET_CLIPBOARD_NOTIFIED', true)
+        }
     }
 }
 
