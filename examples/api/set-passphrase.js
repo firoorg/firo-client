d905d7748061327e036d8dc4be56ff65e7c39e8f
riordant <riordant@tcd.ie>
2018-07-10 16:33:20 +0100

    add set-passphrase example call

---
+++ examples/api/set-passphrase.js
@@ -0,0 +1,20 @@
+const zmq = require('zeromq')
+
+const requester = zmq.socket('req')
+requester.connect('tcp://127.0.0.1:25557')
+
+// log out replies
+requester.on('message', (msg) => {
+    console.log('received message')
+    console.log(JSON.parse(msg.toString()))
+})
+
+// send stringified json
+requester.send(JSON.stringify({
+    type: 'create',
+    collection: 'set-passphrase',
+    data: {
+        "oldpassword": "password",
+        "newpassword": "12345"
+    }
+}))
