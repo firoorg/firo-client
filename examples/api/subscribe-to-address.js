9633545680b1a9c28fe2fe69cea951958d4f1611
riordant <riordant@tcd.ie>
2018-07-14 12:24:08 +0100

    add block subscribing

--- examples/api/subscribe-to-address.js
+++ examples/api/subscribe-to-address.js
@@ -4,9 +4,16 @@ const subscriber = zmq.socket('sub')
 subscriber.connect('tcp://127.0.0.1:28332')
 
 subscriber.subscribe('address')
+subscriber.subscribe('block')
 
 subscriber.on('message', (topic, message) => {
     console.log('received message from address publisher')
+    console.log('topic', topic.toString())
+    console.log('message', message ? JSON.parse(message.toString()) : message)
+})
+
+subscriber.on('message', (topic, message) => {
+    console.log('received message from block publisher')
     console.log('topic', topic.toString())
     console.log('message', message ? JSON.parse(message.toString()) : message)
 })
