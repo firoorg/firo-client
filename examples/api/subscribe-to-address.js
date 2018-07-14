fe2751cd31660a0e54d0ec04a907e5d472285eb9
Jörn Röder <kontakt@joernroeder.de>
2018-07-14 20:53:13 +0200

    one listener to rule them all

--- examples/api/subscribe-to-address.js
+++ examples/api/subscribe-to-address.js
@@ -7,13 +7,7 @@ subscriber.subscribe('address')
 subscriber.subscribe('block')
 
 subscriber.on('message', (topic, message) => {
-    console.log('received message from address publisher')
-    console.log('topic', topic.toString())
-    console.log('message', message ? JSON.parse(message.toString()) : message)
-})
-
-subscriber.on('message', (topic, message) => {
-    console.log('received message from block publisher')
+    console.log('received message from publisher')
     console.log('topic', topic.toString())
     console.log('message', message ? JSON.parse(message.toString()) : message)
 })
