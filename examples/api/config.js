fdd26233f3946e7fd1b9eb244ef395a8175c3a19
riordant <riordant@tcd.ie>
2018-07-16 16:53:02 +0100

    fixed config file bug

--- examples/api/config.js
+++ examples/api/config.js
@@ -4,8 +4,8 @@ config.auth = '7';
 config.noauth = '8';
 
 config.mainnet = '1555';
-config.testnet += '2555';
-config.testnet ++= '3555';
+config.testnet = '2555';
+config.regtest = '3555';
 
 config.password = '12345';
 
