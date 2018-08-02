5a3d60c4290046bb496563ad52aa760b132a46f7
Jörn Röder <kontakt@joernroeder.de>
2018-08-02 23:44:52 +0200

    building zcoin core on all cores 🚀

--- fetch-and-build-zcoin-core.sh
+++ fetch-and-build-zcoin-core.sh
@@ -5,6 +5,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 REPO_NAME=zcoin-repo
 REPO=$DIR/$REPO_NAME
 
+NUMBER_OF_PROCESSORS=1
+# @link https://stackoverflow.com/a/47594483/520544
+CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu || echo "$NUMBER_OF_PROCESSORS")
+
 if [ ! -d "$REPO" ]; then
     # clone the repo
     git clone --branch client-api https://github.com/zcoinofficial/zcoin/ $REPO
@@ -17,11 +21,11 @@ git pull
 # compile
 ./autogen.sh
 ./configure --with-boost-libdir=/usr/local/Cellar/boost/1.67.0_1/lib/
-make
+make -j${CORES}
 
-# move to gui assets
-ls -lh
-
+# copy binaries to gui static folder
 cp $REPO/src/zcoind $DIR/static/core
 cp $REPO/src/zcoin-cli $DIR/static/core
 cp $REPO/src/zcoin-tx $DIR/static/core
+
+ls -lh $DIR/static/core
