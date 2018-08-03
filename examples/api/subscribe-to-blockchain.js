00cbc69d3853fa529368079390d322ebf349cbd3
Jörn Röder <kontakt@joernroeder.de>
2018-08-03 12:09:12 +0200

    seperated address and block subscription examples

--- examples/api/subscribe-to-blockchain.js
+++ examples/api/subscribe-to-blockchain.js
@@ -4,7 +4,7 @@ const config = require("./config")
 const subscriber = zmq.socket('sub')
 subscriber.connect('tcp://127.0.0.1:' + config.sub)
 
-subscriber.subscribe('address')
+subscriber.subscribe('block')
 
 subscriber.on('message', (topic, message) => {
     console.log('received message from publisher')
