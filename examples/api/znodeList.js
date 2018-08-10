c5d9d4727a1c6a2d876542cc9a0f2c7c7a3c65b7
riordant <riordant@tcd.ie>
2018-08-10 16:05:37 +0100

    add znodelist example

--- examples/api/znodeList.js
+++ examples/api/znodeList.js
@@ -2,7 +2,7 @@ const config = require("./config")
 const zmq = require('zeromq')
 
 const requester = zmq.socket('req')
-requester.connect('tcp://127.0.0.1:15557')
+requester.connect('tcp://127.0.0.1:' + (TESTNET ? config.testnet : config.regtest) + config.auth)
 
 // log out replies
 requester.on('message', (msg) => {
