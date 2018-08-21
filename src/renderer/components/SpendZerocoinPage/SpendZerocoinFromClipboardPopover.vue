7834f9f9509373794ff98501ad01ab83b874f007
Jörn Röder <kontakt@joernroeder.de>
2018-08-21 16:46:26 +0200

    added SpendZerocoinFromClipboard popover

---
+++ src/renderer/components/SpendZerocoinPage/SpendZerocoinFromClipboardPopover.vue
@@ -0,0 +1,99 @@
+<template>
+    <base-popover
+            :open="isOpen"
+            placement="left-start"
+            :popover-class="[ isUsedAddress ? 'warning' : '' ]"
+            :boundaries-element="boundariesElement"
+            class="send-from-clipboard-popover advice"
+            trigger="manually"
+    >
+        <template slot="target">
+            <slot />
+        </template>
+
+        <template slot="content">
+            <transition name="fade" mode="out-in">
+                <component :is="newOrUsedAddressComponent"
+                           :key="clipboardAddress"
+                           :mark-as-notified="markAsNotified"
+                           :set-send-form-fields="setSpendFormFields" />
+            </transition>
+        </template>
+    </base-popover>
+</template>
+
+<script>
+    import { mapGetters, mapActions } from 'vuex'
+    import types from '~/types'
+    // import { convertToCoin } from '#/lib/convert'
+
+    import SpendZerocoinFromClipboardPopoverNewAddress from '@/components/SpendZerocoinPage/SpendZerocoinFromClipboardPopoverNewAddress'
+    import SpendZerocoinFromClipboardPopoverUsedAddress from '@/components/SpendZerocoinPage/SpendZerocoinFromClipboardPopoverUsedAddress'
+
+    export default {
+        name: 'SpendZerocoinFromClipboardPopover',
+        components: {
+            SpendZerocoinFromClipboardPopoverNewAddress,
+            SpendZerocoinFromClipboardPopoverUsedAddress
+        },
+
+        props: [
+            'boundariesElement'
+        ],
+
+        computed: {
+            ...mapGetters({
+                clipboardHasNewAddress: 'Clipboard/hasNewAddress',
+                clipboardAddress: 'Clipboard/address',
+                clipboardAmount: 'Clipboard/amount',
+                currentFormAddress: 'ZerocoinSpend/spendFormAddress',
+                hasAlreadySentToAddress: 'Address/hasAlreadySentToAddress'
+            }),
+
+            isOpen () {
+                return this.clipboardHasNewAddress &&
+                       this.clipboardAddress !== this.currentFormAddress
+            },
+
+            isUsedAddress () {
+                return this.hasAlreadySentToAddress(this.clipboardAddress)
+            },
+
+            newOrUsedAddressComponent () {
+                const newOrUsed = this.isUsedAddress ? 'Used' : 'New'
+
+                // todo add case to match "send to yourself"
+
+                return `SpendZerocoinFromClipboardPopover${newOrUsed}Address`
+            }
+        },
+
+        methods: {
+            ...mapActions({
+                setFormAddress: types.zerocoinspend.SET_FORM_ADDRESS,
+                // setFormAmount: types.zerocoinspend.SET_FORM_AMOUNT,
+                markAsNotified: types.clipboard.MARK_AS_NOTIFIED
+            }),
+
+            setSpendFormFields () {
+                if (!this.clipboardAddress) {
+                    return
+                }
+
+                this.setFormAddress(this.clipboardAddress)
+
+                /*
+                if (this.clipboardAmount) {
+                    this.setFormAmount(convertToCoin(this.clipboardAmount))
+                }
+                */
+
+                this.markAsNotified()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
