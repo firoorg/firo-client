cab036f9e4d895ae7e61d48900759173cad9dde8
Jörn Röder <kontakt@joernroeder.de>
2018-10-21 18:58:08 +0200

    always stop the daemon

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -2,7 +2,7 @@ export default {
     protocolIdentifier: 'zcoin',
     core: {
         autoRestart: true,
-        stopOnQuit: process.env.NODE_ENV === 'production',
+        stopOnQuit: true, // process.env.NODE_ENV === 'production',
         heartbeatIntervalInSeconds: 10
     },
     folders: {
