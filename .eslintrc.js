946ad283ed4949b80348828b0163a707e16c5d8c
Jörn Röder <kontakt@joernroeder.de>
2018-12-06 16:37:10 +0100

    updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,30 @@
 module.exports = {
     root: true,
-    parser: 'babel-eslint',
     parserOptions: {
+        parser: 'babel-eslint',
         sourceType: 'module'
     },
     env: {
         browser: true,
         node: true
     },
-    extends: 'standard',
+    extends: 'plugin:vue/recommended',
     globals: {
         __static: true
     },
-    plugins: [
-        'html'
-    ],
-    'rules': {
+    rules: {
         'indent': ["error", 4],
+        'vue/html-indent': ['error', 4],
+
         // allow paren-less arrow functions
         'arrow-parens': 0,
         // allow async-await
         'generator-star-spacing': 0,
-        // allow debugger during development
+
+        'vue/no-v-html': 0,
+        'vue/component-name-in-template-casing': 'kebab-case',
+
+// allow debugger during development
         'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
     }
 }
