2edd58416632c1bfe82a0d76acd7ea8c3a8214c0
riordant <riordant@tcd.ie>
2018-12-15 17:45:24 +0100

    Add stop example

---
+++ examples/api/stop.js
@@ -0,0 +1,17 @@
+const config = require("./config")
+const zmq = require('zeromq')
+
+const requester = zmq.socket('req')
+requester.connect('tcp://127.0.0.1:' + (TESTNET ? config.testnet : config.regtest) + config.auth)
+
+// log out replies
+requester.on('message', (msg) => {
+    console.log('received message')
+    console.log(JSON.parse(msg.toString()))
+})
+
+// send stringified json
+requester.send(JSON.stringify({
+    type: 'initial',
+    collection: 'stop'
+}))
