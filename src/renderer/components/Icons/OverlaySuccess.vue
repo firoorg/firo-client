e93e223acd17faf5ec66c2d4a320a4a901e2503d
Jörn Röder <kontakt@joernroeder.de>
2018-12-20 20:14:35 +0100

    added overlay success icon

---
+++ src/renderer/components/Icons/OverlaySuccess.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="overlay-success-animation">
+        <lottie :options="defaultOptions" />
+    </div>
+</template>
+
+<script>
+import Lottie from 'vue-lottie'
+import animationData from '@/assets/animations/overlay-success-white.json'
+
+export default {
+    name: 'OverlaySuccess',
+    components: {
+        Lottie
+    },
+    props: {
+        onAnimationEnd: {
+            type: Function,
+            default: () => {}
+        }
+    },
+
+    data () {
+        return {
+            defaultOptions: {
+                animationData,
+                loop: false,
+                autoplay: true
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
