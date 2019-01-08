71f26013f555c030f9749677b86968f7cf21ddef
riordant <riordant@tcd.ie>
2019-01-08 18:06:06 +0100

    Add settings examples

---
+++ examples/api/setting-update.js
@@ -0,0 +1,27 @@
+const config = require("./config")
+const zmq = require('zeromq')
+
+const requester = zmq.socket('req')
+requester.connect('tcp://127.0.0.1:' + (TESTNET ? config.testnet : config.regtest) + config.auth)
+
+// log out replies
+requester.on('message', (msg) => {
+    console.log('received message')
+    console.log(JSON.parse(msg.toString()))
+})
+
+// send stringified json
+requester.send(JSON.stringify({
+    type: 'update',
+    collection: 'setting',
+    data: {
+        "newsetting": {
+            data: "testupdate",
+            restartRequired: true
+        },
+        "setting_two": {
+            data: "STRING",
+            restartRequired: false
+        }
+    }
+}))
