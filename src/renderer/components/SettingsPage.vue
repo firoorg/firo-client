58ae6e8e32deefec0d21007014ca5641505d2baa
Jörn Röder <kontakt@joernroeder.de>
2019-01-14 12:21:10 +0100

    started settings page layout

--- src/renderer/components/SettingsPage.vue
+++ src/renderer/components/SettingsPage.vue
@@ -1,7 +1,39 @@
 <template>
     <section class="settings-page">
-        <h1>Settings</h1>
-        <p>tbc</p>
+        <div class="window-height">
+            <div v-scrollable>
+                <div class="settings-page-inner">
+                    <h1>{{ $t('settings.overview.title') }}</h1>
+
+                    <section class="interface">
+                        <h2>{{ $t('settings.form.interface.title') }}</h2>
+                        <div class="form">
+                            <div class="field">
+                                <label>
+                                    {{ $t('settings.form.interface.label__language') }}
+                                </label>
+                                <span class="description">
+                                    {{ $t('settings.form.interface.description__language') }}
+                                </span>
+                                <div class="control">
+                                    <input type="text">
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <label>{{ $t('settings.form.interface.label__explorer-pattern') }}</label>
+                                <span class="description">
+                                    {{ $t('settings.form.interface.description__explorer-pattern') }}
+                                </span>
+                                <div class="control">
+                                    <input type="text">
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -13,6 +45,28 @@ export default {
 
 <style lang="scss" scoped>
     .settings-page {
-        padding: emRhythm(5);
+        //padding: emRhythm(5);
+        box-sizing: border-box;
+    }
+
+    .settings-page-inner {
+        padding: emRhythm(5) emRhythm(4);
+    }
+
+    .interface .form {
+        display: flex;
+        justify-content: space-between;
+
+        .field {
+            flex-grow: 1;
+
+            & + .field {
+                margin-left: emRhythm(5);
+            }
+
+            .control {
+                padding: 0 emRhythm(2);
+            }
+        }
     }
 </style>
