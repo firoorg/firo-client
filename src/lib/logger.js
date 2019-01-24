ad6fe2b699fab04ff27b5d9e3b1e5871a264aad3
Jörn Röder <kontakt@joernroeder.de>
2019-01-24 17:10:25 +0100

    added winston logger

---
+++ src/lib/logger.js
@@ -0,0 +1,26 @@
+import { join } from 'path'
+import winston from 'winston'
+import { getApp } from '#/lib/utils'
+
+const logger = winston.createLogger({
+    format: winston.format.combine(
+        //winston.format.colorize(),
+        winston.format.splat(),
+        winston.format.timestamp(),
+        winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+            filename: join(getApp().getPath('userData'), 'combined.log'),
+            handleExceptions: true
+        })
+    ]
+})
+
+export const createLogger = function (opts = { label: 'foo' }) {
+    console.log(logger)
+
+    return logger
+    //return logger.child({ label })
+}
