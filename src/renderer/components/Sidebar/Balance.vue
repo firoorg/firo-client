86c211af3bd2ab337413b111eba562f51fb52ed6
sproxet <sproxet@users.noreply.github.com>
2019-03-31 15:29:19 +0700

    Remove impotent code from Sidebar/Balance.vue.

--- src/renderer/components/Sidebar/Balance.vue
+++ src/renderer/components/Sidebar/Balance.vue
@@ -4,23 +4,13 @@
 </i18n>
 
 <template>
-    <section
-        class="balance"
-        :class="{'is-confirmed': isConfirmed }"
-    >
+    <section class="balance" >
         <div class="label">
             {{ $t('navigation.balance.title') }}
             <span />
         </div>
-        <!-- TODO animate change of current balance
-        @inspiration https://josex2r.github.io/jQuery-SlotMachine/
-        @inspiration http://preview.codecanyon.net/item/jcountdown-mega-package/full_screen_preview/3443480
-        -->
-        <span
-            class="amount"
-            @click="toggleBalance"
-        >
-            {{ balance }}&#8201;XZC
+        <span class="amount">
+            {{ convertToCoin(availableTotal) }}
         </span>
     </section>
 </template>
@@ -28,100 +18,18 @@
 <script>
 import { mapGetters } from 'vuex' /* , mapActions */
 import { convertToCoin } from '#/lib/convert'
-// import ConfirmationBadge from '@/components/Badge/ConfirmationBadge'
 
 export default {
     name: 'Balance',
-    components: {
-        // ConfirmationBadge
-    },
-
-    data () {
-        return {
-            pendingConfirmations: 1,
-            requiredConfirmations: 6,
-            confirmationsInterval: null,
-            showBalanceInCurrency: false,
-            exchange: {
-                factor: 50,
-                unit: 'USD',
-                decimals: 2
-            }
-        }
-    },
-
-    /*
-      created () {
-          this.confirmationsInterval = setInterval(() => {
-              this.pendingConfirmations--
-
-              if (this.pendingConfirmations < 0) {
-                  this.pendingConfirmations = this.requiredConfirmations
-              }
-          }, 10000)
-      },
-
-      beforeDestroy () {
-          clearInterval(this.confirmationsInterval)
-      },
-      */
 
     computed: {
         ...mapGetters({
-            totalBalance: 'Balance/total'
+            availableTotal: 'Balance/availableTotal'
         }),
-
-        isConfirmed () {
-            return this.pendingConfirmations === 0
-        },
-
-        balance () {
-            const {factor, unit, decimals} = this.exchange
-
-            return this.showBalanceInCurrency ? `${(this.totalBalance * factor).toFixed(decimals)} ${unit}` : `${convertToCoin(this.totalBalance)}`
-        }
-    },
-
-    watch: {
-        isConfirmed (isConfirmed, wasConfimed) {
-            if (!isConfirmed) {
-                return
-            }
-
-            /*
-              this.sendNotification({
-                  title: 'Balance Confirmed',
-                  message: 'Your balance is now fully confirmed 👍',
-                  type: 'success'
-              })
-              */
-        },
-        pendingConfirmations (newPending, oldPending) {
-            if (this.isConfirmed) {
-                return
-            }
-
-            // console.log(`Confirmations update ${this.requiredConfirmations - newPending}/${this.requiredConfirmations}. @todo`)
-
-            /*
-              this.showMessage({
-                  message: `Confirmations update ${this.requiredConfirmations - newPending}/${this.requiredConfirmations}. @todo`
-              })
-              */
-        }
     },
 
     methods: {
-        /*
-          ...mapActions({
-              showMessage: 'Notification/showMessage',
-              sendNotification: 'Notification/sendNotification'
-          }),
-          */
-
-        toggleBalance () {
-            // this.showBalanceInCurrency = !this.showBalanceInCurrency
-        }
+        convertToCoin
     }
 }
 </script>
@@ -144,23 +52,5 @@ export default {
         transition: all .15s ease-in-out;
         //text-shadow: 0 0 10px $color--green-bright;
         cursor: default;
-    }
-
-    .pending-confirmations {
-        cursor: help;
-        opacity: 1;
-        transition: opacity .5s 3s ease-in-out;
-    }
-
-    .is-confirmed {
-        .amount {
-            opacity: 1;
-            animation: animate--text-shadow-green-bright 1.5s infinite alternate;
-        }
-
-        .pending-confirmations {
-            opacity: 0;
-            transition-delay: 0;
-        }
     }
 </style>
