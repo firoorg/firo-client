af145620db823437b3554f5b7f9a18e8905a4fbc
sproxet <sproxet@users.noreply.github.com>
2019-06-01 14:50:28 +0700

    Remove 'XZC' from the error tooltip about send/spend amount exceeding available balance. It is used for both XZC and Zerocoin.

--- src/renderer/utils/validationRules/notExceedingBalance.js
+++ src/renderer/utils/validationRules/notExceedingBalance.js
@@ -4,7 +4,7 @@ import { convertToSatoshi, convertToCoin } from '#/lib/convert'
 Validator.extend('not_exceeding_balance', {
     getMessage: (field, { limit }) => {
         const limitAsBaseCoin = convertToCoin(limit)
-        return `The amount your have entered exceeds your available balance of ${limitAsBaseCoin} XZC`
+        return `The amount your have entered exceeds your available balance of ${limitAsBaseCoin}`
     },
 
     validate: (value, { limit }) => {
