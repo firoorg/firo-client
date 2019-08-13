bfdd041db218822c3142b51d9559753ee0eaafa1
sproxet <sproxet@users.noreply.github.com>
2019-08-14 06:15:52 +0700

    Handle "Loading block index" issues on startup.

--- src/daemon/modules/paymentRequest.ts
+++ src/daemon/modules/paymentRequest.ts
@@ -5,7 +5,21 @@ const logger = createLogger('zcoin:daemon:address');
 
 
 export async function initialize(store: any, zcoind: Zcoind) {
-    const data = await zcoind.send(null, 'initial', 'paymentRequest', {});
-    logger.info("Got initial paymentRequest data");
-    store.dispatch('PaymentRequest/setStateWithInitialPaymentRequest', data);
+    while (true) {
+        try {
+            const data = await zcoind.send(null, 'initial', 'paymentRequest', {});
+            logger.info("Got initial paymentRequest data");
+            store.dispatch('PaymentRequest/setStateWithInitialPaymentRequest', data);
+            return;
+        } catch (e) {
+            // Error -28 is "Loading block index..."
+            if (e.error && e.error.code && e.error.code === -28) {
+                logger.warn("initial/paymentRequest returned error -28, loading block index. Trying again in 1s.");
+                store.dispatch('PaymentRequest/setStateWithInitialPaymentRequest', {});
+                await new Promise((resolve) => setTimeout(resolve, 2000)); // sleep 2s
+            } else {
+                throw e;
+            }
+        }
+    }
 }
--- src/daemon/modules/stateWallet.ts
+++ src/daemon/modules/stateWallet.ts
@@ -5,7 +5,20 @@ const logger = createLogger('zcoin:daemon:address');
 
 
 export async function initialize(store: any, zcoind: Zcoind) {
-    const data = await zcoind.send(null, 'initial', 'stateWallet', {});
-    logger.info("Got stateWallet data");
-    store.dispatch('Transactions/setWalletState', data);
+    while (true) {
+        try {
+            const data = await zcoind.send(null, 'initial', 'stateWallet', {});
+            store.dispatch('Transactions/setWalletState', data);
+            return;
+        } catch (e) {
+            // Error -28 is "Loading block index..."
+            if (e.error && e.error.code && e.error.code === -28) {
+                logger.warn("initial/stateWallet returned error -28, loading block index. Trying again in 1s.");
+                store.dispatch('Transactions/setWalletState', {addresses: {}});
+                await new Promise((resolve) => setTimeout(resolve, 2000)); // sleep 2s
+            } else {
+                throw e;
+            }
+        }
+    }
 }
