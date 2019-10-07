602843452c8c8709ed355f42935908ede311c751
sproxet <sproxet@users.noreply.github.com>
2019-10-07 17:25:30 +0700

    Sync Znode list on startup in addition to other times.

--- src/daemon/modules/znodeList.ts
+++ src/daemon/modules/znodeList.ts
@@ -1,8 +1,25 @@
 import { Zcoind } from '../zcoind';
 
-// In addition to initializing the znode list, this function goes into an infinite loop refreshing the data every 5
-// minutes.
 export async function handleEvent(store: any, zcoind: Zcoind, data: any) {
     store.dispatch('Znode/setStateWithZnodeList', data.nodes);
     store.dispatch('Znode/setZnodeCount', data.total);
+}
+
+export async function initialize(store: any, zcoind: Zcoind) {
+    try {
+        const data = await zcoind.send(null, 'initial', 'znodeList', null);
+
+        if (Object.keys(data.nodes).length) {
+            store.dispatch('Znode/setStateWithZnodeList', data.nodes);
+            store.dispatch('Znode/setZnodeCount', data.total);
+        }
+    } catch (e) {
+        if (e.error && e.error.code === -28) {
+            // We haven't loaded the block index yet. We don't need to do anything, because a push event will be sent
+            // instead.
+            return;
+        }
+
+        throw e;
+    }
 }
