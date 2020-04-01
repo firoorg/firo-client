b60e2b96c003ea7c7c19db51ddf039a12104c12d
sproxet <sproxet@users.noreply.github.com>
2020-04-01 18:44:37 +0700

    Remove unused PaymentStepPassphrase component.

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -88,11 +88,6 @@ sync(store, router, {
 // Allow users to access the store from Chrome Dev Tools.
 window.$store = store;
 
-// This is so we can pass around global events, which we're basically using just to allow enter to work to submit forms
-// that are spread across a large number of files with mixins.
-// TODO: Figure out how to get rid of this.
-Vue.prototype.$eventHub = new Vue()
-
 /// Start up zcoind.
 zcoind(store)
     .then(z => {
