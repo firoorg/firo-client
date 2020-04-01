eafd4b450cdfcf2d362cbebaf75bf2d3a4f6ced9
sproxet <sproxet@users.noreply.github.com>
2020-04-01 17:19:49 +0700

    Make new balance module more robust.

--- src/daemon/modules/balance.ts
+++ src/daemon/modules/balance.ts
@@ -1,10 +1,27 @@
 import { Zcoind } from '../zcoind';
 
+import { createLogger } from '../../lib/logger';
+const logger = createLogger('zcoin:daemon:balance');
+
 export async function handleEvent(store: any, zcoind: Zcoind, data: any) {
     store.dispatch('Balance/updateBalance', data);
 }
 
 export async function initialize(store: any, zcoind: Zcoind) {
-    const data = await zcoind.send(null, 'get', 'balance', null);
-    store.dispatch('Balance/updateBalance', data);
+    while (true) {
+        try {
+            const data = await zcoind.send(null, 'get', 'balance', null);
+            logger.info("Got initial balance data");
+            store.dispatch('Balance/updateBalance', data);
+            return;
+        } catch (e) {
+            // Error -28 is "Loading block index..."
+            if (e.error && e.error.code && e.error.code === -28) {
+                logger.warn("get/balance returned error -28, loading block index. Trying again in 1s.");
+                await new Promise((resolve) => setTimeout(resolve, 2000)); // sleep 2s
+            } else {
+                throw e;
+            }
+        }
+    }
 }
