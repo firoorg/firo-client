a4ff08c6d4c8aa749918000f650d9ee48062fd2c
sproxet <sproxet@users.noreply.github.com>
2020-04-14 17:29:57 +0700

    Fix up initial settings request.

--- src/daemon/modules/settings.ts
+++ src/daemon/modules/settings.ts
@@ -4,7 +4,9 @@ import { createLogger } from '../../lib/logger';
 const logger = createLogger('zcoin:daemon:settings');
 
 export async function initialize(store: any, zcoind: Zcoind) {
+    await zcoind.awaitBlockIndex();
+
     const data = await zcoind.getSettings();
     logger.info("Got initial settings data");
-    store.dispatch('Settings/setDaemonSettings', data);
+    store.commit('Settings/setDaemonSettings', data);
 }
