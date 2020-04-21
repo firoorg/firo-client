4619da802de5fe44425157d76468a5f47aae85fb
sproxet <sproxet@users.noreply.github.com>
2020-04-21 11:26:01 +0700

    Make IntroScreenLockWallet call initializers only once, and improve logging there.

--- src/renderer/components/IntroScreen/IntroScreenLockWallet.vue
+++ src/renderer/components/IntroScreen/IntroScreenLockWallet.vue
@@ -22,7 +22,6 @@
 </template>
 
 <script>
-import Vue from 'vue';
 import { mapGetters } from 'vuex'
 
 import GuideStepMixin from '@/mixins/GuideStepMixin'
@@ -32,6 +31,7 @@ import IntroScreenLockWalletCreate from './IntroScreenLockWalletCreate'
 import IntroScreenLockWalletConfirm from './IntroScreenLockWalletConfirm'
 import IntroScreenLockWalletWarning from './IntroScreenLockWalletWarning'
 
+import {Zcoind} from "#/daemon/zcoind";
 import zcoind from "#/daemon/init";
 const app = require("electron").remote.app;
 
@@ -98,47 +98,67 @@ export default {
         async lockWallet() {
             const mnemonic = this.actions.getCachedMnemonic();
 
+            let initialDaemon;
             try {
-                window.$daemon = await zcoind(this.$store, this.zcoinClientNetwork, this.zcoindLocation, this.blockchainLocation, mnemonic);
+                this.$log.info("Starting zcoind with mnemonic and no initializers or handlers...");
+                initialDaemon = new Zcoind(this.zcoinClientNetwork, this.zcoindLocation, this.blockchainLocation, [], {});
+                await initialDaemon.start(mnemonic);
             } catch(e) {
-                this.$log.error(`Failed to start zcoind: ${e}`);
-                alert(`Failed to start zcoind: ${e}`);
+                this.$log.error(`Failed to start zcoind with mnemonic: ${e}`);
+                alert(`Failed to start zcoind with mnemonic: ${e}`);
                 app.exit(-1);
             }
 
-            await $daemon.awaitInitializersCompleted();
+            await initialDaemon.awaitBlockIndex();
 
             try {
                 this.$log.info("Trying to lock the wallet... This will shutdown zcoind.");
                 // This call will shutdown zcoind.
-                await $daemon.setPassphrase(null, this.passphrase);
+                await initialDaemon.setPassphrase(null, this.passphrase);
             } catch(e) {
+                this.$log.error("Something unexpected went wrong with locking the wallet, so we can't proceed.");
                 alert("Something unexpected went wrong with locking the wallet, so we can't proceed. Please report this to the Zcoin team.");
                 app.exit(-1);
             }
 
             this.$log.info("Waiting for zcoind to stop listening so we can restart it...");
-            await $daemon.awaitZcoindNotListening();
+            await initialDaemon.awaitZcoindNotListening();
 
-            this.$log.info("Restarting zcoind...");
-            await $daemon.start();
+            this.$log.info("Starting zcoind normally...");
+            try {
+                this.$log.info("Starting zcoind with mnemonic and no initializers or handlers...");
+                window.$daemon = await zcoind(this.$store, this.zcoinClientNetwork, this.zcoindLocation, this.blockchainLocation);
+            } catch(e) {
+                this.$log.error(`Failed to start zcoind normally: ${e}`);
+                alert(`Failed to start zcoind normally: ${e}`);
+                app.exit(-1);
+            }
 
-            this.$log.info("Waiting for initializers to complete...");
-            await $daemon.awaitInitializersCompleted();
+            try {
+                this.$log.info("Waiting for initializers to complete...");
+                await $daemon.awaitInitializersCompleted();
+            } catch(e) {
+                this.$log.error(`initializers failed to complete: ${e}`);
+                alert(`initializers failed to complete: ${e}`);
+                app.exit(-1);
+            }
 
+            this.$log.info("Sanity checking mnemonic...");
             const mnemonicSanityCheck = await $daemon.showMnemonics(this.passphrase);
             if (mnemonicSanityCheck !== mnemonic.mnemonic) {
                 // This should never happen.
-                alert("Mnemonic sanity check failed; this is a bug in the client.\n" +
+                this.$log.error("Mnemonic sanity check failed. This is a bug.");
+                alert("Mnemonic sanity check failed; this is a bug in the client.\n\n" +
                       `Mnemonic we tried to set: ${mnemonic.mnemonic}\n` +
-                      `Mnemonic zcoind gave us: ${mnemonicSanityCheck}\n` +
+                      `Mnemonic zcoind gave us: ${mnemonicSanityCheck}\n\n` +
                        "Seek help from the Zcoin team; do not try to use the client again."
                 );
                 app.exit(-1);
             }
             this.$log.info("Mnemonic sanity check passed.");
 
-            $store.commit('App/setIsInitialized', true);
+            this.$log.info("Marking app as initialized...");
+            this.$store.commit('App/setIsInitialized', true);
             // We will be destroyed automatically from MainLayout.
         },
     }
