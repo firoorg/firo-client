d9ba4eb27bd985fea021841369da2e8e193a06dd
sproxet <sproxet@signaleleven.dev>
2020-08-16 20:02:55 +0700

    Remove more cruft from dev-runner.js.

--- electron-vue/dev-runner.js
+++ electron-vue/dev-runner.js
@@ -4,48 +4,19 @@ const { spawn } = require('child_process');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 
-const mainConfig = require('./webpack.main.config');
 const rendererConfig = require('./webpack.renderer.config');
 
-async function startRenderer () {
-    rendererConfig.entry.renderer = [path.join(__dirname, 'dev-client')].concat(rendererConfig.entry.renderer);
-    rendererConfig.mode = 'development';
+rendererConfig.entry.renderer = [path.join(__dirname, 'dev-client')].concat(rendererConfig.entry.renderer);
+rendererConfig.mode = 'development';
 
-    const compiler = webpack(rendererConfig);
-    const server = new WebpackDevServer(
-        compiler,
-        {
-            contentBase: path.join(__dirname, '../'),
-            hot: true
-        }
-    )
+const compiler = webpack(rendererConfig);
+const server = new WebpackDevServer(
+    compiler,
+    {
+        contentBase: path.join(__dirname, '../'),
+        hot: true
+    }
+);
+server.listen(9080);
 
-    server.listen(9080)
-}
-
-async function startMain () {
-    mainConfig.entry.main = [path.join(__dirname, '../src/main/index.dev.js')].concat(mainConfig.entry.main);
-    mainConfig.mode = 'development';
-
-    webpack(mainConfig);
-}
-
-function startElectron () {
-    const electronProcess = spawn(electron, ['--inspect=5858', path.join(__dirname, '../dist/electron/main.js')])
-
-    electronProcess.on('close', () => {
-        process.exit()
-    })
-}
-
-function init () {
-    Promise.all([startRenderer(), startMain()])
-        .then(() => {
-            startElectron()
-        })
-        .catch(err => {
-            console.error(err)
-        })
-}
-
-init()
+spawn(electron, ['--inspect=5858', path.join(__dirname, '../dist/electron/main.js')]);
