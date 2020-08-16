f1ac2eaa7a541666bd89369e59574ebe296819d1
sproxet <sproxet@signaleleven.dev>
2020-08-16 20:23:12 +0700

    Make WebPack hot reloading work properly.

--- src/main/index.js
+++ src/main/index.js
@@ -75,7 +75,8 @@ app.once('ready', async () => {
 
     if (process.env.NODE_ENV === 'development') {
         logger.info("Loading development environment at localhost:9080...");
-        await ourWindow.loadURL("http://localhost:9080/");
+        // ?reload=true is required for WebPack hot reloading to work without a manual refresh.
+        await ourWindow.loadURL("http://localhost:9080/?reload=true");
     } else {
         const indexDotHtml = join(__dirname, 'index.html');
         logger.info(`Loading production environment at ${indexDotHtml}...`);
