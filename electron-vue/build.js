5a5b425dbe6dc9ee1dfd50a761b6e0d5a366d594
sproxet <sproxet@signaleleven.dev>
2020-09-01 13:17:16 +0700

    Make `npm run build` display build errors.

--- electron-vue/build.js
+++ electron-vue/build.js
@@ -3,15 +3,32 @@ const webpack = require('webpack');
 const mainConfig = require('./webpack.main.config');
 const rendererConfig = require('./webpack.renderer.config');
 
+const promises = [];
+
 for (const config of [mainConfig, rendererConfig]) {
-  console.log(`building ${config.target} module...`);
+    config.mode = 'production';
 
-  webpack(config, (err) => {
-    if (err) {
-      console.error(`${config.target} module failed to build: ${err}`);
-      process.exit(1)
-    }
+    console.info(`building ${config.target} module...`);
 
-    console.log(`${config.target} module built successfully...`);
-  });
+    promises.push(new Promise((resolve, reject) => webpack(config, (err, stats) => {
+        const errors = [...(err ? [err] : []), ...stats.compilation.errors, ...stats.compilation.warnings];
+
+        if (errors.length > 1) {
+            console.error(`${config.target} failed to build:`);
+            for (const e of errors) {
+                console.error(e);
+            }
+            reject();
+        } else {
+            console.info(`${config.target} module built successfully...`);
+            resolve();
+        }
+    })));
 }
+
+Promise.all(promises).catch(() => {
+    console.error('compilation failed; aborting...');
+    // FIXME: Some hooks are added somewhere so that process.exit() doesn't actually work. This generates a nasty error
+    //        message and core dump, but meh.
+    process.abort();
+});
