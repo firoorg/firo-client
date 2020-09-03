b571f5be7a095c2ea3354c8d1e7b0b1a6b44a906
sproxet <sproxet@signaleleven.dev>
2020-09-03 15:56:06 +0700

    Log errors for build failures.

--- electron-vue/build.js
+++ electron-vue/build.js
@@ -3,10 +3,11 @@ const rendererConfig = require('./webpack.renderer.config');
 const compileWrapper = require('./compile-wrapper');
 
 module.exports = Promise.all([mainConfig, rendererConfig].map((c) => {
-    c.mode = process.env.NODE_ENV || 'production';
+    c.mode = 'production';
     return compileWrapper(c);
-})).catch(() => {
+})).catch((e) => {
     console.error('compilation failed; aborting...');
+    console.error(e);
     // FIXME: Some hooks are added somewhere so that process.exit() doesn't actually work. This generates a nasty error
     //        message and core dump, but meh.
     process.abort();
