d5fe29ae63ad13033c8000372a2d548148f76a2e
sproxet <sproxet@signaleleven.dev>
2020-09-12 19:22:32 +0700

    Make test logic more local.

--- tests/spec.ts
+++ tests/spec.ts
@@ -17,7 +17,6 @@ if (process.env.BUILD_ZCOIN_CLIENT !== 'false') {
 }
 
 const passphrase = 'passphrase';
-let twelveMnemonicWords = ["nation","tip","mean","govern","tide","comic","figure","gift","upper","love","kitchen","dolphin"];
 let mnemonicWords: string[];
 
 function scaffold(this: Mocha.Suite, reinitializeZcoinClient: boolean) {
@@ -59,15 +58,6 @@ function scaffold(this: Mocha.Suite, reinitializeZcoinClient: boolean) {
         this.timeout(10e3);
         await this.app.stop();
     });
-}
-
-async function generateBlocks(self: This, blocks: number) {
-    await (await self.app.client.$('a[href="#/debugconsole"]')).click();
-
-    await self.app.client.keys([...`generate ${blocks}`.split(''), "Enter"]);
-    await self.app.client.waitUntil(async () =>
-        (await (await self.app.client.$('#current-input')).getText()) === ''
-    , {timeout: Math.max(2000, blocks*500)});
 }
 
 describe('Regtest Setup', function (this: Mocha.Suite) {
@@ -185,6 +175,8 @@ describe('Regtest Setup', function (this: Mocha.Suite) {
     it('can recover from mnemonics', async function (this: This) {
         this.timeout(1000e3);
 
+        let twelveMnemonicWords = ["nation","tip","mean","govern","tide","comic","figure","gift","upper","love","kitchen","dolphin"];
+
         await (await this.app.client.$('#recover-from-mnemonic')).click();
 
         const submitButton = await this.app.client.$('#submit-button');
@@ -246,11 +238,17 @@ describe('Regtest Setup', function (this: Mocha.Suite) {
         await (await this.app.client.$('.tx-page')).waitForExist({timeout: 60e3});
     });
 
-    it('generates XZC', async function (this: This) {
+    it('generates XZC from the debug console', async function (this: This) {
         this.timeout(500e3);
         this.slow(100e3);
 
-        await generateBlocks(this, 500);
+        await (await this.app.client.$('a[href="#/debugconsole"]')).click();
+
+        await this.app.client.keys([..."generate 500".split(''), "Enter"]);
+        await this.app.client.waitUntil(
+            async () => (await (await this.app.client.$('#current-input')).getText()) === '',
+            {timeout: 250e3}
+        );
 
         await this.app.client.waitUntilTextExists('#available-xzc', '16843');
         await this.app.client.waitUntilTextExists('#pending-xzc', '4300');
@@ -259,9 +257,6 @@ describe('Regtest Setup', function (this: Mocha.Suite) {
 
 describe('Opening an Existing Wallet', function (this: Mocha.Suite) {
     scaffold.bind(this)(false);
-
-    let nonce = String(Math.random());
-    let receiveAddress: string;
 
     it('loads our existing wallet', async function (this: This) {
         this.timeout(20e3);
@@ -276,15 +271,17 @@ describe('Opening an Existing Wallet', function (this: Mocha.Suite) {
         const receiveAddressElement = await this.app.client.$('#receive-address');
         await receiveAddressElement.waitForExist();
 
-        receiveAddress = await receiveAddressElement.getText();
-        await this.app.client.executeAsyncScript(`$daemon.legacyRpc('generatetoaddress 1 ${receiveAddress}').then(arguments[0])`, []);
-        await this.app.client.waitUntil(async () => (await receiveAddressElement.getText()) !== receiveAddress);
-        receiveAddress = await receiveAddressElement.getText();
+        let originalReceiveAddress = await receiveAddressElement.getText();
+        await this.app.client.executeAsyncScript(`$daemon.legacyRpc('generatetoaddress 1 ${originalReceiveAddress}').then(arguments[0])`, []);
+        await this.app.client.waitUntil(async () => (await receiveAddressElement.getText()) !== originalReceiveAddress);
     });
 
     it('sends and receives a public payment', async function (this: This) {
         this.timeout(60e3);
         this.slow(30e3);
+
+        let nonce = String(Math.random());
+        let receiveAddress = await this.app.client.executeAsyncScript(`$daemon.getUnusedAddress().then(arguments[0])`, []);
 
         await (await this.app.client.$('a[href="#/send/public')).click();
         await (await this.app.client.$('.send-zcoin-form')).waitForExist();
