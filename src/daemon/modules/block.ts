2ed72cee4cde5e36511148d692e4a1a61af19a3f
sproxet <sproxet@signaleleven.dev>
2021-01-07 14:41:23 +0700

    Fix Blockchain/currentBlockHeight by listening for both 'block' and 'blockchain' events.

--- src/daemon/modules/block.ts
+++ src/daemon/modules/block.ts
@@ -1,14 +1,5 @@
 import { Firod } from '../firod';
 
-import { createLogger } from '../../lib/logger';
-const logger = createLogger('firo:daemon:blockchain');
-
 export async function handleEvent(store: any, firod: Firod, data: any) {
-    store.commit('Blockchain/updateBlockchain', data);
-}
-
-export async function initialize(store: any, firod: Firod) {
-    const data = await firod.send(null, 'get', 'blockchain', null);
-    logger.info("Got initial blockchain data");
     store.commit('Blockchain/updateBlockchain', data);
 }
