8995c09e8854ee9238eb5a40db0ba2c4d9e64a5e
sproxet <sproxet@signaleleven.dev>
2021-04-12 12:44:32 +0700

    Don't needlessly write config information that's already been set.

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -243,7 +243,7 @@ window.$startDaemon = () => new Promise(resolve => {
                 logger.info("firod has started.");
                 $setWaitingReason(undefined);
                 store.commit('App/setFirodHasStarted', true);
-                await store.dispatch('App/setIsInitialized', true);
+                if (!$store.getters['App/isInitialized']) await store.dispatch('App/setIsInitialized', true);
                 resolve();
             } else {
                 // Direct the user to the lock wallet screen. We will never resolve(), but that shouldn't matter.
