f039d30175bb823148bc16c7ddacf22b8225c3b2
sproxet <me@sproxet.dev>
2021-09-05 16:10:45 +0700

    Fix styles for SendPage/ErrorStep.

--- src/renderer/components/SendPage/ErrorStep.vue
+++ src/renderer/components/SendPage/ErrorStep.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="error-step">
+    <div class="info-popup error-step">
         <div class="title">Error</div>
 
         <div class="content">
@@ -7,7 +7,7 @@
         </div>
 
         <div class="buttons">
-            <button class="ok-button" @click="$emit('ok')">
+            <button class="solid-button recommended ok-button" @click="$emit('ok')">
                 Ok
             </button>
         </div>
