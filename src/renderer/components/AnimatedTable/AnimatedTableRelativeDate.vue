115b0c6ba7a349553966d93f1a10eaae8713c501
sproxet <me@sproxet.dev>
2022-05-23 13:20:13 +0700

    Fix an issue where CoinSwap entries were shown as unconfirmed.

--- src/renderer/components/AnimatedTable/AnimatedTableRelativeDate.vue
+++ src/renderer/components/AnimatedTable/AnimatedTableRelativeDate.vue
@@ -6,8 +6,8 @@
     >
         Date
     </th>
-    <td v-else-if="!rowData.blockHeight && !rowData.isInstantSendLocked" class="unconfirmed">Unconfirmed</td>
     <td v-else-if="rowData.date" class="confirmed">{{createDate}}</td>
+    <td v-else-if="!rowData.blockHeight && !rowData.isInstantSendLocked" class="unconfirmed">Unconfirmed</td>
     <td
         v-else
         class="vuetable-td-component-relative-date"
