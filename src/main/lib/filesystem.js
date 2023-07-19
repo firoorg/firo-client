1a5fc5174dcbb173a922aba124a70b2df103845c
sproxet <17163658+sproxet@users.noreply.github.com>
2023-07-19 19:01:10 +0700

    Upgrade directory selection dialogs.

--- src/main/lib/filesystem.js
+++
@@ -1,8 +0,0 @@
-import electron from 'electron'
-const dialog = electron.dialog
-
-export const selectDirectory = function (window) {
-    dialog.showOpenDialog(window, {
-        properties: ['openDirectory']
-    })
-}
