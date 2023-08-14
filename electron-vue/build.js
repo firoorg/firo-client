20cf4c3e9f79900af535633e1be342e9a1cd87d7
sproxet <17163658+sproxet@users.noreply.github.com>
2023-08-14 15:42:18 +0545

    Updated electron-vue/build.js with new webpack config file.

--- electron-vue/build.js
+++ electron-vue/build.js
@@ -1,14 +1,15 @@
-const mainConfig = require('./webpack.main.config');
-const rendererConfig = require('./webpack.renderer.config');
+const config = require('../webpack.config');
 const compileWrapper = require('./compile-wrapper');
 
-module.exports = Promise.all([mainConfig, rendererConfig].map((c) => {
-    c.mode = 'production';
-    return compileWrapper(c);
-})).catch((e) => {
-    console.error('compilation failed; aborting...');
-    console.error(e);
-    // FIXME: Some hooks are added somewhere so that process.exit() doesn't actually work. This generates a nasty error
-    //        message and core dump, but meh.
-    process.abort();
-});
+module.exports = [async () => {
+    try {
+        config.mode = 'production';
+        return compileWrapper(config);
+    } catch (e) {
+        console.error('compilation failed; aborting...');
+        console.error(e);
+        // FIXME: Some hooks are added somewhere so that process.exit() doesn't actually work. This generates a nasty error
+        //        message and core dump, but meh.
+        process.abort();
+    }
+}];
