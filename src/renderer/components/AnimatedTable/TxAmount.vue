46363849fffb2ce37fb82c82ea550992dc4907f0
sproxet <17163658+sproxet@users.noreply.github.com>
2023-09-11 17:03:35 +0700

    Fix amounts not showing in coin control list.

--- src/renderer/components/AnimatedTable/TxAmount.vue
+++ src/renderer/components/AnimatedTable/TxAmount.vue
@@ -6,14 +6,19 @@
 </template>
 
 <script>
-import VuetableFieldMixin from 'vue3-vuetable/src/components/VuetableFieldMixin.vue'
+import VuetableFieldMixin from 'vue3-vuetable/src/components/VuetableFieldMixin.vue';
+import Amount from 'renderer/components/shared/Amount.vue';
 
 export default {
     name: 'TxAmount',
 
     mixins: [
         VuetableFieldMixin
-    ]
+    ],
+
+    components: {
+        Amount
+    }
 }
 
 </script>
