fb1c3f42dc99e23e64ef5bac36440f4457962a3b
sproxet <17163658+sproxet@users.noreply.github.com>
2023-09-29 14:50:51 +0700

    Work on SwapZone support. Unfinished.

--- src/lib/coinswap.ts
+++ src/lib/coinswap.ts
@@ -527,10 +527,167 @@ export class StealthExApi extends AbstractCoinSwapApi {
     }
 }
 
+export class SwapZoneApi extends AbstractCoinSwapApi {
+    API: string = 'https://api.swapzone.io/v1/exchange';
+    provider: Provider = 'SwapZone';
+    pairs: [Ticker, Ticker][];
+
+    constructor(apiKey: ApiKey) {
+        super();
+        this.apiKey = apiKey;
+    }
+
+    async getPairs(): Promise<[Ticker, Ticker][]> {
+        if (this.pairs)
+            return this.pairs;
+
+        const r = await axios.get(`${this.API}/currencies`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': this.apiKey
+            }
+        });
+
+        this.pairs = r.data
+            .map(x => x.ticker)
+            .sort()
+            .reduce((a, x) => x == a[a.length-1] || x == 'firo' ? a : [...a, x], [])
+            .map(ticker => [['FIRO', ticker.toUpperCase()], [ticker.toUpperCase(), 'FIRO']])
+            .reduce((a, x) => [...a, ...x], []);
+
+        return this.pairs;
+    }
+
+    async getPairInfo(from: Ticker, to: Ticker, amount: bigint): Promise<PairInfo> {
+        if (!amount)
+            throw new Error('amount must be specified');
+
+        const r = await axios.get(`${this.API}/get-rate?from=${from.toLowerCase()}&to=${to.toLowerCase()}&amount=${bigintToString(amount)}&rateType=fixed`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': this.apiKey
+            }
+        });
+
+        if (r.data.error)
+            throw new Error(r.data.message);
+
+        const toReceive = stringToBigint(String(r.data.amountTo));
+        if (!toReceive)
+            throw 'invalid response';
+
+        const rate = (toReceive * 10n**8n) / amount;
+
+        return {
+            provider: this.provider,
+            quoteId: r.data.quotaId,
+            from,
+            to,
+            rate,
+            min: stringToBigint(String(r.data.minAmount)),
+            max: stringToBigint(String(r.data.maxAmount)),
+            fee: 0n
+        };
+    }
+
+    async getOrderStatus(orderId: OrderId): Promise<OrderInfo> {
+        const r = await axios.get(`${this.API}/exchange/${orderId}?api_key=${this.apiKey}`);
+
+        const rate = stringToBigint(r.data.amount_from) * 10n ** 8n / stringToBigint(r.data.amount_to);
+
+        return {
+            provider: this.provider,
+
+            sendAmount: stringToBigint(r.data.amount_from),
+            refundAddress: r.data.refund_address,
+            destinationAddress: r.data.address_to,
+
+            from: r.data.currency_from.toUpperCase(),
+            to: r.data.currency_to.toUpperCase(),
+            rate: rate,
+            fee: 0n,
+
+            orderId: r.data.id,
+            exchangeAddress: r.data.address_to,
+            status: r.data.status,
+            receiveAmount: stringToBigint(r.data.amount_to),
+
+            fromTxId: r.data.tx_from || undefined,
+            toTxId: r.data.tx_to || undefined,
+            refundTxId: undefined,
+
+            createdAt: Date.parse(r.data.timestamp),
+            receivedAt: undefined,
+            updatedAt: Date.parse(r.data.updated_at),
+            validUntil: Date.parse(r.data.timestamp) + 60 * 60 * 24
+        };
+    }
+
+    async makeOrder(order: Order): Promise<OrderInfo> {
+        const response = await axios.post(`${this.API}/create`, {
+            from: order.pairInfo.from.toLowerCase(),
+            to: order.pairInfo.to.toLowerCase(),
+            addressReceive: order.destinationAddress,
+            refundAddress: order.refundAddress,
+            amountDeposit: Number(bigintToString(order.sendAmount)),
+            quotaId: order.pairInfo.quoteId
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': this.apiKey
+            }
+        });
+
+        if (response.data.error)
+            throw new Error(response.data.message);
+
+        console.log(response);
+        throw 'x';
+
+        if (response.data?.refund_address != order.refundAddress ||
+            response.data?.address_to != order.destinationAddress ||
+            response.data?.currency_from.toUpperCase() != order.pairInfo.from ||
+            response.data?.currency_to.toUpperCase() != order.pairInfo.to
+        ) throw new Error("response doesn't match request");
+
+        const receiveAmount = stringToBigint(response.data.amount_to);
+        if (!receiveAmount)
+            throw new Error('invalid response');
+
+        // If the amount we're supposed to receive is less than the expected amount by more than 1 FIRO, throw an error
+        // and refuse to continue.
+        const expectedReceiveAmount = order.sendAmount * order.pairInfo.rate / (10n**8n);
+        if (expectedReceiveAmount - order.pairInfo.rate > receiveAmount)
+            throw new Error(`We expected to receive ${bigintToString(expectedReceiveAmount)} ` +
+                `${order.pairInfo.to} but got a promise of only ${bigintToString(receiveAmount)}.`)
+
+        return {
+            provider: this.provider,
+
+            sendAmount: order.sendAmount,
+            refundAddress: order.refundAddress,
+            destinationAddress: order.destinationAddress,
+
+            from: order.pairInfo.from,
+            to: order.pairInfo.to,
+            rate: order.pairInfo.rate,
+            fee: order.pairInfo.fee,
+
+            orderId: response.data.id,
+            exchangeAddress: response.data.address_from,
+            status: 'waiting',
+            receiveAmount,
+
+            createdAt: Math.floor(Date.now() / 1000)
+        };
+    }
+}
+
 const ApiClasses = {
     ChangeNow: ChangeNowApi,
     Exolix: ExolixApi,
-    StealthEx: StealthExApi
+    StealthEx: StealthExApi,
+    SwapZone: SwapZoneApi
 };
 export const PROVIDERS: Provider[] = <Provider[]>Object.keys(ApiClasses);
 
